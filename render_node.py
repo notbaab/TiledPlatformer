ebb67d3d4e355091a3620b508768ccbab1aaed16
etinlb <etinlb@gmail.com>
2014-10-10 12:30:43 -0700

    Running local host tries until a connect
    No more Address in use errors

--- render_node.py
+++ render_node.py
@@ -1,10 +1,17 @@
 import networking as n
 import displayplatformer
+import time
 
 # TODO: Figure out tile location based on hostname
 if __name__ == '__main__':
   game = displayplatformer.ClientPlatformer([0, 0], [200,0])
-  server = n.Server('localhost', 2000, game)
+  connected = False
+  while not connected:
+    try:
+      server = n.Server('localhost', 2000, game)
+      connected = True
+    except Exception:
+      time.sleep(.5)
   print(server.socket)
   server.open_connection()  # open and receive the first data packet
   while True:
